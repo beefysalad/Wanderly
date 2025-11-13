010a93cca8fa55ff0a8aa6420f662b3944962fc3
beefysalad <jpatrickzephyr@gmail.com>
2025-11-14 05:09:06 +0800

    fix: Build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,8 @@
 import LandingPage, { IUserCredentials } from "./components/LandingPage";
 
 export default function Home() {
-  const handleLogin = (userCredentials: IUserCredentials) => {
-    console.log(userCredentials);
-  };
-  const handleRegister = (userCredentials: IUserCredentials) => {
-    console.log(userCredentials);
-  };
   const handleQuickJoin = (code: string, guestName: string) => {
     console.log(code, guestName);
   };
-  return (
-    <LandingPage
-      onLogin={handleLogin}
-      onRegister={handleRegister}
-      onQuickJoin={handleQuickJoin}
-    />
-  );
+  return <LandingPage onQuickJoin={handleQuickJoin} />;
 }
