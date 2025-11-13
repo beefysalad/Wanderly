0449f5b7acede8727351593b56715904603cf027
beefysalad <jpatrickzephyr@gmail.com>
2025-11-13 17:12:41 +0800

    feat: add Prisma

---
+++ prisma/migrations/20251113090419_add_user_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "firebaseId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firebaseId_key" ON "User"("firebaseId");
